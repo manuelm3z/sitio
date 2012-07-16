836138bb02c1dced669807118d49b6112040d12b
Manuel Zambrano <mmzmm3z@gmail.com>
2012-07-16 10:26:25 -0430

    Segunda version de clase Datos en datos.php

--- lib/core/datos.php
+++ lib/core/datos.php
@@ -1,25 +1,35 @@
 <?php
-/************************************************
-* Autor: @manuelm3z                             *
-* Documentación: esta clase se encarga de       *
-* conectar con el servidor de base de datos     *                             *
-************************************************/
+/*
+* Autor: @manuelm3z
+* Documentación: esta clase se encarga de conectar con el 
+* servidor de base de datos
+*/
 include '../settings.php';
 class Datos{
 	public $conexion;
-	public $baseDatos;	
+	public $baseDatos;
+	public $respuesta;	
 		
 	function __construct(){
 		$this->conexion=(@mysql_connect(server,user,pass));
 		$this->baseDatos=(@mysql_select_db(bd,$this->conexion));
 
 		if(!$this->conexion){
-		echo 'No se puede conectar al servidor';
+			$this->respuesta = "No se puede conectar al servidor";
+			return $this->respuesta;
 		}elseif(!$this->baseDatos){
-			echo 'No se puede conectar a la Base de Datos';
-			}
+			$this->respuesta = "no se encuentra la base de datos, creando...";
+			return $this->respuesta;
+			$consulta = mysql_query("CREATE DATABASE ".bd);
+			foreach($tablas as $ejecutar){
+				$consulta = mysql_query($ejecutar);
+				}
+		}
 	}
-	//retornará true si la conexion está establecida
+
+	/*
+	* retornará true si la conexion está establecida
+	*/
 	public function comprobarConexion(){
 		if($this->conexion && $this->baseDatos){
 			return true;
--- lib/settings.php
+++ lib/settings.php
@@ -1,77 +1,113 @@
 <?php
-/************************************************
-* Autor: @manuelm3z                             *
-* Documentación: en este documento están las    *
-* configuraciones de base de datos.             *
-************************************************/
+/*
+* Autor: @manuelm3z
+* Documentación: en este documento están las configuraciones 
+* de base de datos.
+*/
 
-// servidor de la base de datos
+/*
+* servidor de la base de datos
+*/
 define("server", "localhost");
-// usuario de la base de datos
+
+/*
+* usuario de la base de datos
+*/
 define("user", "root");
-// password de la base de datos
+
+/*
+* password de la base de datos
+*/
 define("pass", "naci21-09");
-// base de dato donde se obtendran los valores
+
+/*
+* base de dato donde se obtendran los valores
+*/
 define("bd", "codearagua");
 
-//Estas es la descripción de las tablas
-$talleres = "create table talleres(
-	id int(11) auto_increment primary key,
-	nombre varchar(30) not null,
-	apellido varchar(50) not null,
-	cedula int(8) not null,
+/*
+* Estas es la descripción de las tablas
+*/
+$talleres = "CREATE TABLE IF NOT EXISTS talleres(
+	id int(11) AUTO_INCREMENT PRIMARY KEY,
+	nombre varchar(30) NOT NULL,
+	apellido varchar(50) NOT NULL,
+	cedula int(8) NOT NULL,
 	twitter varchar(50),
-	email varchar(50) not null,
-	asistencia int(1) default 0,
-	formulario varchar(20) not null
+	email varchar(50) NOT NULL,
+	asistencia int(1) DEFAULT 0,
+	formulario varchar(20) NOT NULL
 	);";
 
-$medios = "";
-$imagenes = "";
-//tabla creada para almacenar los datos de los usuarios.
-$usuario = "create table usuario(
-	id_usuario int(11) auto_increment primary key,
-	log_usuario varchar(30) not null,
-	clave_usuario varchar(50) not null,
+/*
+* tabla creada para almacenar los datos de los usuarios.
+*/
+$usuario = "CREATE TABLE IF NOT EXISTS usuario(
+	id_usuario int(11) AUTO_INCREMENT PRIMARY KEY,
+	log_usuario varchar(30) NOT NULL,
+	clave_usuario varchar(50) NOT NULL,
 	nombre_usuario varchar(30),
 	apellido_usuario varchar(50),
-	email_usuario varchar(50) not null,
-	fecha_registro date not null
+	email_usuario varchar(50) NOT NULL,
+	fecha_registro date NOT NULL
 	);";
 
-//tabla creada para almacenar los tipos de permisos admitidos.
-$permisos = "create table permisos(
-	id_permiso int(11) auto_increment primary key,
-	permiso text not null
+/*
+* tabla creada para almacenar los tipos de permisos admitidos.
+*/
+$permisos = "CREATE TABLE IF NOT EXISTS permisos(
+	id_permiso int(11) auto_increment PRIMARY KEY,
+	permiso text NOT NULL
 	);";
 
-//tabla donde se almacenan las asociaciones de permisos a cada usuario.
-$permisos_usuarios = "create table permisos_usuarios(
-	id int(11) auto_increment primary key,
-	id_usuario int(4) not null,
-	id_permiso int(4) not null
+/*
+* tabla donde se almacenan las asociaciones de permisos a cada usuario.
+*/
+$permisos_usuarios = "CREATE TABLE IF NOT EXISTS permisos_usuarios(
+	id int(11) auto_increment PRIMARY KEY,
+	id_usuario int(4) NOT NULL,
+	id_permiso int(4) NOT NULL
 	);";
 
-//tabla que almacena los post del blog del sitio.
-$blog_post = "create table blog_post(
-id_post int(11) auto_increment primary key,
-titulo_post varchar(255) not null,
-contenido_post longtext not null,
-estado_post int(1) not null,
-id_usuario int(4) not null,
-fecha_post datetime not null
-);";
+/*
+* tabla que almacena los post del blog del sitio.
+*/
+$blog_post = "CREATE TABLE IF NOT EXISTS blog_post(
+	id_post int(11) auto_increment PRIMARY KEY,
+	titulo_post varchar(255) NOT NULL,
+	contenido_post longtext NOT NULL,
+	estado_post int(1) NOT NULL,
+	id_usuario int(4) NOT NULL,
+	fecha_post datetime NOT NULL
+	);";
 
-//tabla que almacena las etiquetas de los post.
-$etiquetas_post = "create table etiquetas_post(
-	id_etiqueta int(11) auto_increment primary key,
+/*
+*tabla que almacena las etiquetas de los post.
+*/
+$etiquetas_post = "CREATE TABLE IF NOT EXISTS etiquetas_post(
+	id_etiqueta int(11) auto_increment PRIMARY KEY,
 	etiqueta varchar(20)
 	);";
 
-//tabla que almacena la relación de las etiquetas a los post.
-$blog_post_etiquetas = "create table blog_post_etiquetas(
-	id int(11) auto_increment primary key,
-	id_post int(4) not null,
-	id_etiqueta int(4) not null
+/*
+* tabla que almacena la relación de las etiquetas a los post.
+*/
+$blog_post_etiquetas = "CREATE TABLE IF NOT EXISTS blog_post_etiquetas(
+	id int(11) auto_increment PRIMARY KEY,
+	id_post int(4) NOT NULL,
+	id_etiqueta int(4) NOT NULL
 	);";
+
+/*
+* Array que contiene la lista de tablas de la base de datos,
+* agregar el nombre de las variables que guardan las tablas, para ser verificadas.
+*/
+$tablas = array('talleres' => $talleres,
+	'usuario' => $usuario,
+	'permisos' => $permisos,
+	'permisos_usuarios' => $permisos_usuarios,
+	'blog_post' => $blog_post,
+	'etiquetas_post' => $etiquetas_post,
+	'blog_post_etiquetas' => $blog_post_etiquetas,
+	);
 ?>
