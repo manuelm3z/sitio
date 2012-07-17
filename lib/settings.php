cb3e68dc117b5cf9cd077e5fa300b27e1fc904ff
Manuel Zambrano <mmzmm3z@gmail.com>
2012-07-17 14:23:48 -0430

    modificación de las talas de la base de datos

--- lib/settings.php
+++ lib/settings.php
@@ -29,74 +29,81 @@ define("bd", "codearagua");
 * Estas es la descripción de las tablas
 */
 $talleres = "CREATE TABLE IF NOT EXISTS talleres(
-	id int(11) AUTO_INCREMENT PRIMARY KEY,
+	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nombre varchar(30) NOT NULL,
 	apellido varchar(50) NOT NULL,
 	cedula int(8) NOT NULL,
 	twitter varchar(50),
 	email varchar(50) NOT NULL,
-	asistencia int(1) DEFAULT 0,
-	formulario varchar(20) NOT NULL
+	asistencia tinyint(1) NOT NULL,
+	formulario varchar(20) NOT NULL,
+	INDEX(cedula),
+	INDEX(formulario)
 	);";
 
 /*
 * tabla creada para almacenar los datos de los usuarios.
 */
 $usuario = "CREATE TABLE IF NOT EXISTS usuario(
-	id_usuario int(11) AUTO_INCREMENT PRIMARY KEY,
-	log_usuario varchar(30) NOT NULL,
-	clave_usuario varchar(50) NOT NULL,
-	nombre_usuario varchar(30),
-	apellido_usuario varchar(50),
-	email_usuario varchar(50) NOT NULL,
-	fecha_registro date NOT NULL
+	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	usuario varchar(30) NOT NULL UNIQUE KEY,
+	nombre varchar(30), apellido varchar(30),
+	email varchar(50) NOT NULL,
+	fecha_registro datetime NOT NULL,
+	ultima_conexion datetime NOT NULL,
+	estado tinyint(1) NOT NULL
 	);";
 
 /*
 * tabla creada para almacenar los tipos de permisos admitidos.
 */
-$permisos = "CREATE TABLE IF NOT EXISTS permisos(
-	id_permiso int(11) auto_increment PRIMARY KEY,
-	permiso text NOT NULL
+$permiso = "CREATE TABLE IF NOT EXISTS permiso(
+	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nombre text NOT NULL
 	);";
 
 /*
 * tabla donde se almacenan las asociaciones de permisos a cada usuario.
 */
-$permisos_usuarios = "CREATE TABLE IF NOT EXISTS permisos_usuarios(
-	id int(11) auto_increment PRIMARY KEY,
-	id_usuario int(4) NOT NULL,
-	id_permiso int(4) NOT NULL
+$usuarios_permisos = "CREATE TABLE IF NOT EXISTS usuarios_permisos(
+	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	usuario_id int(11) NOT NULL,
+	permiso_id int(11) NOT NULL,
+	INDEX(usuario_id),
+	INDEX(permiso_id)
 	);";
 
 /*
 * tabla que almacena los post del blog del sitio.
 */
-$blog_post = "CREATE TABLE IF NOT EXISTS blog_post(
-	id_post int(11) auto_increment PRIMARY KEY,
-	titulo_post varchar(255) NOT NULL,
-	contenido_post longtext NOT NULL,
-	estado_post int(1) NOT NULL,
-	id_usuario int(4) NOT NULL,
-	fecha_post datetime NOT NULL,
-	url_imagen text
+$post = "CREATE TABLE IF NOT EXISTS post(
+	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	titulo varchar(255) NOT NULL UNIQUE KEY,
+	url_imagen text NOT NULL,
+	contenido longtext NOT NULL,
+	estado tinyint(1) NOT NULL,
+	fecha datetime NOT NULL,
+	usuario_id int(11) NOT NULL,
+	INDEX(usuario_id)
 	);";
 
 /*
 *tabla que almacena las etiquetas de los post.
 */
-$etiquetas_post = "CREATE TABLE IF NOT EXISTS etiquetas_post(
-	id_etiqueta int(11) auto_increment PRIMARY KEY,
-	etiqueta varchar(20)
+$etiqueta = "CREATE TABLE IF NOT EXISTS etiqueta(
+	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	etiqueta varchar(20) NOT NULL UNIQUE KEY
 	);";
 
 /*
 * tabla que almacena la relación de las etiquetas a los post.
 */
-$blog_post_etiquetas = "CREATE TABLE IF NOT EXISTS blog_post_etiquetas(
-	id int(11) auto_increment PRIMARY KEY,
-	id_post int(4) NOT NULL,
-	id_etiqueta int(4) NOT NULL
+$post_etiqueta = "CREATE TABLE IF NOT EXISTS post_etiqueta(
+	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	post_id int(11) NOT NULL,
+	etiqueta_id int(11) NOT NULL,
+	INDEX(post_id),
+	INDEX(etiqueta_id)
 	);";
 
 /*
@@ -105,10 +112,10 @@ $blog_post_etiquetas = "CREATE TABLE IF NOT EXISTS blog_post_etiquetas(
 */
 $tablas = array('talleres' => $talleres,
 	'usuario' => $usuario,
-	'permisos' => $permisos,
-	'permisos_usuarios' => $permisos_usuarios,
-	'blog_post' => $blog_post,
-	'etiquetas_post' => $etiquetas_post,
-	'blog_post_etiquetas' => $blog_post_etiquetas,
+	'permiso' => $permiso,
+	'usuarios_permisos' => $usuarios_permisos,
+	'post' => $post,
+	'etiqueta' => $etiqueta,
+	'post_etiqueta' => $blog_post_etiquetas,
 	);
 ?>
