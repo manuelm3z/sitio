60df220a6168a8573faf9307d21870af154ff9e9
Manuel Zambrano <mmzmm3z@gmail.com>
2012-07-18 13:50:44 -0430

    comentarios en usuario.php

--- lib/core/usuario.php
+++ lib/core/usuario.php
@@ -21,6 +21,7 @@ class Usuario{
 	function __construct($inLog=null, $inClave=null){
 		//Verificar si las variables están vacias antes de asignarlas.
 		if(!empty($inLog) && !empty($inClave)){
+			//realizo la verificación en base de datos que el registro exista.
 			if($consulta = mysql_query("SELECT * FROM usuario WHERE usuario='".$inLog."';")){
 				$resultado = mysql_fetch_assoc($consulta);
 				if($inClave == $resultado["clave"]){
@@ -33,10 +34,15 @@ class Usuario{
 					$this->fecha_registro = $this->fecha($resultado["fecha_registro"]);
 					$this->ultima_conexion = $this->fecha($resultado["ultima_conexion"]);
 					$this->estado = $resultado["estado"];
-					if($_SESSION['usuario']=$this->usuario){
-						$this->mensaje = "Sesion iniciada";
+					//verficar si el usuario está activo
+					if($this->estado == 1){
+						if($_SESSION['usuario']=$this->usuario){
+							$this->mensaje = "Sesion iniciada";
+						}else{
+							$this->mensaje = "No se pudo iniciar la sesion";
+						}
 					}else{
-						$this->mensaje = "No se pudo iniciar la sesion";
+						$this->mensaje = "Usuario Desactivado";
 					}
 				}else{
 					$this->mensaje = "Contraseña incorrecta.";
@@ -49,12 +55,18 @@ class Usuario{
 		}
 	}
 
+	/*
+	* Este método se encarga de formatear la fecha y hora recibida de base de datos.
+	*/
 	private function fecha($fecha){
 		$reemplazo = str_replace("-", " ", $fecha);
 		$dividoFecha = explode(" ", $reemplazo);
 		return $dividoFecha[2]."/".$dividoFecha[1]."/".$dividoFecha[0]." ".$dividoFecha[3];
 	}
-
+	
+	/*
+	* Este método regresa un mensaje si la variable introducida está vacía.
+	*/
 	private function vacio($valor){
 		if(!$valor){
 			return "No esta registrado";
