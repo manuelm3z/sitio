bcf17f4fea0966b4eeef66f744bb94c9b4ea9ef8
Manuel Zambrano <mmzmm3z@gmail.com>
2012-10-02 22:22:36 -0430

    formulario de solicitud de plan

--- contenido/solicitud.php
+++ contenido/solicitud.php
@@ -1,1 +1,11 @@
-Aquí deberia ir la cuestión del formulario
+<form id="fs">
+	Nombre:<input type="text" name="nombre"><br>
+	Apellido:<input type="text" name="apellido"><br>
+	Email:<input type="email" name="email"><br>
+	Plan:<select name="plan">
+			<option value="Basico">Básico</option>
+			<option value="Intermedio">Intermedio</option>
+			<option value="Avanzado">Avanzado</option>
+		 </select>
+	<input type="button" value="Solicitar" onclick="registro(this)">
+</form>
