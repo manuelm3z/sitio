cbbb3db522f1b972c08c07a3f2a803116781eb75
Manuel Zambrano <mmzmm3z@gmail.com>
2012-10-12 23:19:10 -0430

    acomodado la instanciacion de la clase

--- contenido/mail.php
+++ contenido/mail.php
@@ -1,57 +1,12 @@
 <?php
+include('../lib/core/host.php');
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $email = $_POST['email'];
 $plan = $_POST['plan'];
 
-$zonaHoraria = date('e');//zona horaria
-$dia = date('d');//dia
-$mes = date('m');//mes
-$ano = date('Y');//año
-echo "Dia ".$dia." Mes ".$mes." Año ".$ano;
-print $nombre;
-print $apellido;
-print $email;
-print $plan;
-/*$asunto = "Comprobante de Solicitud"; 
-$cuerpo = ' 
-<html> 
-<head> 
-   <title>Comprobante de Solicitud</title> 
-</head> 
-<body> 
-<h1>Estimado(a) '.$destinatario.'</h1> 
-<p>Para procesar su solicitud debe realizar un deposito o transferencia a las siguientes cuentas:<br>
-Banco: Banco Occidental de Descuento
-Nombre: Augusto Lozada
-Cuenta Ahorro: 0116-0203-73-0197391095<br><br>
+$solicitud=new Host($nombre, $apellido, $email, $plan);
+$solicitud->enviarCorreo();
 
-Banco: Bancaribe
-Nombre: Manuel Zambrano
-Cuenta Ahorro: 0114-0201-18-2011298659<br><br>
-
-Banco: Banesco Banco Universal
-Nombre: Manuel Zambrano
-Cuenta Ahorro: 0134-0142-02-1422163960<br><br>
-
-Luego, enviar un correo electronico a ventas@codearagua.com, el comprobante de transferencia o deposito.<br><br>
-
-Mensaje generado '.$dia.'/'.$mes.'/'.$ano.'
-</p> 
-</body> 
-</html> 
-'; 
-//para el envío en formato HTML 
-$headers = "MIME-Version: 1.0\r\n"; 
-$headers .= "Content-type: text/html; charset=utf-8\r\n"; 
-
-//dirección del remitente 
-$headers .= "From: CodeAragua <ventas@codearagua.com>\r\n"; 
-
-//dirección de respuesta, si queremos que sea distinta que la del remitente 
-$headers .= "Reply-To: ventas@codearagua.com\r\n"; 
-
-mail($email,$asunto,$cuerpo,$headers)
-*/ 
 
 ?>
