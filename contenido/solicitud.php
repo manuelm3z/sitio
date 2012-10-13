dd5b4e7a9288f71ebb7d94c83a5402354a8fd299
augustolzd <augustolzd@gmail.com>
2012-10-12 21:56:25 -0430

    Estilo de formulario para pedido, y cambio de fuente de letra general

--- contenido/solicitud.php
+++ contenido/solicitud.php
@@ -1,11 +1,13 @@
-<form id="fs" onsubmit="registro(this)">
-	Nombre:<input type="text" name="nombre"><br>
-	Apellido:<input type="text" name="apellido"><br>
-	Email:<input type="email" name="email"><br>
-	Plan:<select name="plan">
+<p class="text-titulos">Solicitud de Hosting</p>
+
+<form id="fs" onsubmit="registro(this)"><br/>
+	<input type="text" name="nombre" placeholder="Nombre" required><br>
+	<input type="text" name="apellido" placeholder="Apellido" required><br>
+	<input type="email" name="email" placeholder="Correo Electronico" required><br><br/>
+	Plan: <select name="plan">
 			<option value="Basico">Básico</option>
 			<option value="Intermedio">Intermedio</option>
 			<option value="Avanzado">Avanzado</option>
 		 </select>
-	<input type="submit" value="Solicitar">
+	<input type="submit" value="Solicitar" id="envio">
 </form>
